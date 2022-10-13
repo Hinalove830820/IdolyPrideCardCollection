821c8139234a2c9106db0e71dd621e666fc4216f
夏 <115185721+Hinalove830820@users.noreply.github.com>
2022-10-13 15:47:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function filter() {
 function exportImage() {
   let exportButton = document.getElementById('export')
   let node = document.getElementById('capture');
-  exportButton.innerHTML = '<button onclick="exportImage()">曬卡中</button>'
+  exportButton.innerHTML = '<button onclick="exportImage()">...曬卡中...</button>'
   let oldPics = document.getElementById('uesrCards')
   oldPics.deleteCell()
   let scale = 3
