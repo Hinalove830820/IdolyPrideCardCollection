2db056a645d391af66e098ccc227bba81ad9adc0
Hinalove830820 <hinalove830820@gmail.com>
2022-12-27 20:48:15 +0900

    ver 2.0.0
    優化UI和功能的曬卡小工具v2.0
    
    * 目前僅支援手機開啟
    * 優化了多重篩選功能，可針對角色、屬性、卡池、職業、團隊等功能篩選
    * 優化了儲存圖片功能，點擊按鈕可自動下載
    * 新增了排序功能，目前可針對角色名稱、登場時間作正反排序
    * 新增了小綠勾勾，更好判斷自己選到卡了沒
    * 新增改名功能，可以輸入使用者的Manager Name
    * 新增local storage，可儲存上次編輯的狀況
    * 新增多語言，可切換中文/日文

--- script.js
+++ script.js
@@ -1,172 +1,351 @@
-// 全域參數放在這裡
-// 偶像名稱
-const idols = [
-  'mana',
-  'kotono', 'nagisa', 'saki', 'suzu', 'mei',
-  'sakura', 'sizuku', 'chisa', 'rei', 'haruko',
-  'rui', 'yuu', 'sumire',
-  'rio', 'aoi', 'ai', 'kokoro',
-  'fran', 'kana', 'miho',
-  'miku'
-]
-// 卡片名稱 'name-no-pool.webp'
-const cardPaths = [
-  'mana-01-fes.webp', 'mana-02-fes.webp', 'mana-03-special.webp',
-  'kotono-01-normal.webp', 'kotono-02-limited.webp', 'kotono-03-fes.webp', 'kotono-04-limited.webp', 'kotono-05-limited.webp', 'kotono-06-special.webp',
-  'nagisa-01-normal.webp', 'nagisa-02-normal.webp', 'nagisa-03-normal.webp', 'nagisa-04-limited.webp', 'nagisa-05-limited.webp', 'nagisa-06-limited.webp',
-  'saki-01-normal.webp', 'saki-02-normal.webp', 'saki-03-normal.webp', 'saki-04-limited.webp', 'saki-05-normal.webp', 'saki-06-limited.webp',
-  'suzu-01-normal.webp', 'suzu-02-normal.webp', 'suzu-03-limited.webp', 'suzu-04-limited.webp', 'suzu-05-normal.webp', 'suzu-06-limited.webp',
-  'mei-01-normal.webp', 'mei-02-limited.webp', 'mei-03-limited.webp', 'mei-04-fes.webp', 'mei-05-normal.webp', 'mei-06-limited.webp',
-  'sakura-01-normal.webp', 'sakura-02-fes.webp', 'sakura-03-limited.webp', 'sakura-04-limited.webp',
-  'sizuku-01-normal.webp', 'sizuku-02-normal.webp', 'sizuku-03-limited.webp', 'sizuku-04-normal.webp', "sizuku-05-limited.webp",
-  'chisa-01-normal.webp', 'chisa-02-limited.webp', 'chisa-03-normal.webp', 'chisa-04-limited.webp', 'chisa-05-normal.webp',
-  'rei-01-normal.webp', 'rei-02-normal.webp', 'rei-03-limited.webp', 'rei-04-normal.webp', 'rei-05-limited.webp', 'rei-06-fes.webp',
-  'haruko-01-normal.webp', 'haruko-02-limited.webp', 'haruko-03-limited.webp', 'haruko-04-normal.webp', 'haruko-05-limited.webp', 'haruko-06-normal.webp',
-  'rui-01-normal.webp', 'rui-02-normal.webp', 'rui-03-limited.webp', 'rui-04-normal.webp', 'rui-05-special.webp',
-  'yuu-01-normal.webp', 'yuu-02-limited.webp', 'yuu-03-limited.webp', 'yuu-04-normal.webp', 'yuu-05-fes.webp',
-  'sumire-01-normal.webp', 'sumire-02-normal.webp', 'sumire-03-limited.webp', 'sumire-04-normal.webp', 'sumire-05-fes.webp',
-  'rio-01-normal.webp', 'rio-02-normal.webp', 'rio-03-fes.webp', 'rio-04-limited.webp', 'rio-05-limited.webp',
-  'aoi-01-normal.webp', 'aoi-02-limited.webp', 'aoi-03-limited.webp', 'aoi-04-normal.webp', 'aoi-05-fes.webp',
-  'ai-01-normal.webp', 'ai-02-limited.webp', 'ai-03-limited.webp', 'ai-04-normal.webp', 'ai-05-limited.webp',
-  'kokoro-01-normal.webp', 'kokoro-02-limited.webp', 'kokoro-03-limited.webp', 'kokoro-04-normal.webp',
-  'fran-01-fes.webp', 'fran-02-limited.webp',
-  'kana-01-fes.webp', 'kana-02-limited.webp',
-  'miho-01-fes.webp', 'miho-02-normal.webp',
-  'miku-01-special.webp',
-]
-// 載入控件
-function loadWidget() {
-  // 建立偶像篩選器控件
-  const idolSector = document.querySelector('#idolSection')
-  for (i = 0; i < idols.length; i++) {
-    idolSector.innerHTML += `<div><input type="checkbox" id=${idols[i]} class="avatar" onclick="filter()" checked><label for=${idols[i]} class="idols"><img class="idol" id=${idols[i]} src="avatar/${idols[i]}.png" alt=""></label></div>`
-  }
-  // 建立卡池篩選器控件
-  const cardSector = document.querySelector('#cardSelectSection')
-  for (i = 0; i < cardPaths.length; i++) {
-    let tags = cardPaths[i].split("-")
-    tags[2] = tags[2].split('.')[0]
-    cardSector.innerHTML += `<div><input type="checkbox" id=${tags[0] + tags[1]} class="${'cards ' + tags[0] + ' ' + tags[2]}" onclick='check()' unchecked>  <label for=${tags[0] + tags[1]} class="${'img ' + tags[0] + ' ' + tags[2]}"><img id=${tags[0] + tags[1]} src=${'cards/' + cardPaths[i]} alt=""></label></div>`
-  }
-
+const idolGroup = {
+  sunny: ['sakura', 'sizuku', 'chisa', 'rei', 'haruko'],
+  tsuki: ['kotono', 'nagisa', 'saki', 'suzu', 'mei'],
+  liznoir: ['rio', 'aoi', 'ai', 'kokoro'],
+  trinityaile: ['rui', 'yuu', 'sumire'],
+  iiix: ['fran', 'kana', 'miho']
 }
-// 執行載入控件
-loadWidget()
+const sortingRule = ['↓Name', '↑Name', '↓Date', '↑Date']
+let sortingRuleIndex = 0
 
-// 按下卡片時 計數並結算持有率
-function check() {
-  const cardsIHave = document.querySelector("#totalcards")
-  const normalCardsIhave = document.querySelector("#normalcards")
-  const limitedCardsIhave = document.querySelector("#limitedcards")
-  const fesCardsIhave = document.querySelector("#fescards")
-  let mytotal = 0
-  let totalPercent = 0
-  let mynormal = 0
-  let normalPercent = 0
-  let mylimited = 0
-  let limitedPercent = 0
-  let myfes = 0
-  let fesPercent = 0
-  let totalcards = document.querySelectorAll(".cards");
-  let normalcards = document.querySelectorAll(".cards.normal")
-  let limitedcards = document.querySelectorAll(".cards.limited")
-  let fesCards = document.querySelectorAll(".cards.fes")
-  // 總所持率
-  for (let i = 0; i < totalcards.length; i++) {
-    if (totalcards[i].checked) {
-      mytotal++;
-    }
-  }
-  totalPercent = (mytotal / totalcards.length * 100).toFixed(1)
-  cardsIHave.textContent = `五星卡所持率：${totalPercent} % ( ${mytotal} / ${totalcards.length} )`;
-  // 常駐卡所持率
-  for (let i = 0; i < normalcards.length; i++) {
-    if (normalcards[i].checked) {
-      mynormal++;
-    }
-  }
-  normalPercent = (mynormal / normalcards.length * 100).toFixed(1)
-  normalCardsIhave.textContent = `常駐池所持率：${normalPercent} % ( ${mynormal} / ${normalcards.length} )`;
-  // 限定卡所持率
-  for (let i = 0; i < limitedcards.length; i++) {
-    if (limitedcards[i].checked) {
-      mylimited++;
-    }
-  }
-  limitedPercent = (mylimited / limitedcards.length * 100).toFixed(1)
-  limitedCardsIhave.textContent = `限定池所持率：${limitedPercent} % ( ${mylimited} / ${limitedcards.length} )`;
-  // FES卡所持率
-  for (let i = 0; i < fesCards.length; i++) {
-    if (fesCards[i].checked) {
-      myfes++;
-    }
-  }
-  fesPercent = (myfes / fesCards.length * 100).toFixed(1)
-  fesCardsIhave.textContent = `FES池所持率：${fesPercent} % ( ${myfes} / ${fesCards.length} )`;
+const multiLanguageArray = {
+  jp: ['所持率ﾁｪｯｶｰ', 'CN', '全選択', 'ソート', 'ﾌｨﾙﾀ', '　☆5所持率', '通常ｶﾞﾁｬ', '限定ｶﾞﾁｬ', 'ﾌｪｽｶﾞﾁｬ', '全☆5ｶｰﾄﾞ', 'ｽﾍﾟｼｬﾙ', 'ｽｺｱﾗｰ', 'ﾊﾞｯﾌｧｰ', 'ｻﾎﾟｰﾀｰ', '全取り消す', '...画面制作中...', '数秒かかると予想される', '滑らないでお待ちください'],
+  cn: ['曬卡小工具', 'JP', '全部選擇', '排序', '篩選器', '　的持有率', '常駐池', '限定池', 'FES池', '五星卡', '特殊池', 'scorer', 'buffer', 'support', 'Clear Idols', '...正在產製圖片...', '預計花費數秒至數十秒', '請耐心等候並不要滑動']
+};
+let languageToUse = ''
 
-}
+const newVersion = 'v2.00'
+let recentCards = JSON.parse(localStorage.getItem('recentCards')) || []
+let recentVersion = localStorage.getItem('recentVersion')
+let playerCards = []
+let cardsToRender = []
 
-// 按下篩選器時 未選到的種類卡片透明化
-function filter() {
-  let pools = ['normal', 'limited', 'fes', 'special',]
 
-  let cards = document.querySelectorAll('.img')
+const cardContainer = document.querySelector("#card-container")
+const filterButton = document.querySelector("#filter-button")
+const modal = document.querySelector("#modal")
+const filters = document.querySelectorAll('.filter')
+const nameForm = document.querySelector('#name')
+const player = document.querySelector("#player")
+const input = document.querySelector('#name-input')
+const filterAllButton = document.querySelector("#filter-all")
+const selectAllButton = document.querySelector('#select-all')
+const idolFilters = document.querySelectorAll('.idol-avatar')
+const sorter = document.querySelector('#sorter')
+const languageSelectButton = document.querySelector('#language')
 
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].style.opacity = '100%'
+let playerName = localStorage.getItem('playerName') || 'マネージャー牧野'
+
+// 載入時執行
+filters.forEach((filter) => {
+  filter.dataset.select = 'true'
+})
+
+fetch('data.json')
+  .then((response) => response.json())
+  .then((json) => {
+    playerCards = recentCards.concat(json.slice(recentCards.length))
+    cardsToRender = playerCards
+    renderCards()
+    renderResults()
+    renderName(playerName)
+
+    useLanguage('')
+  })
+
+sorter.addEventListener('click', (event) => {
+
+  sortingRuleIndex = (Number.parseInt(event.target.getAttribute('sort')) + 1) % sortingRule.length
+  event.target.setAttribute('sort', sortingRuleIndex)
+  switch (sortingRuleIndex) {
+    case 0: //↓Idol
+      cardsToRender.sort((a, b) => {
+        return ('' + a.idol).localeCompare(b.idol, 'en')
+      })
+      break
+    case 1: //↑Idol
+      cardsToRender.sort((a, b) => {
+        return ('' + b.idol).localeCompare(a.idol, 'en')
+      })
+      break
+    case 2: //↓Date
+      cardsToRender.sort((a, b) => (Number.parseInt(a.release) - Number.parseInt(b.release))
+      )
+      break
+    case 3: //↑Date
+      cardsToRender.sort((a, b) => (Number.parseInt(b.release) - Number.parseInt(a.release))
+      )
+      break
   }
-  for (let i = 0; i < pools.length; i++) {
-    let cardsToHide = document.querySelectorAll(('.' + pools[i]))
-    let pool = document.querySelector(('#' + pools[i]))
-    if (!pool.checked) {
-      for (let i = 0; i < cardsToHide.length; i++) {
-        cardsToHide[i].style.opacity = '20%'
-      }
-    }
+  sorter.innerText = sortingRule[sortingRuleIndex]
+  renderCards()
 
-  }
-  for (let i = 0; i < idols.length; i++) {
-    let idolsToHide = document.querySelectorAll(('.' + idols[i]))
-    let idol = document.querySelector(('#' + idols[i]))
-    if (!idol.checked) {
-      for (let i = 0; i < idolsToHide.length; i++) {
-        idolsToHide[i].style.opacity = '20%'
-      }
-    }
+
+
+})
+
+player.addEventListener('click', (event) => {
+  nameForm.toggleAttribute('show')
+  input.focus()
+
+})
+
+nameForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (input.value.trim() !== "") {
+    renderName(input.value)
+    playerName = input.value
+    nameForm.toggleAttribute('show')
+    localSave()
+  } else {
+    renderName('マネージャー牧野')
+    playerName = ''
+    nameForm.toggleAttribute('show')
+    localSave()
   }
 
 }
 
-//按下「曬卡」後，產生一張高清曬卡圖
-function exportImage() {
-  let exportButton = document.getElementById('export')
-  let node = document.getElementById('capture');
-  exportButton.innerHTML = '<button onclick="exportImage()">...曬卡中...</button>'
-  // 清掉舊圖
-  let oldImg = document.getElementById('uesrCards')
-  document.body.removeChild(oldImg)
-  // 產生曬卡圖
-  let scale = 3
-  domtoimage.toPng(node
-    //// 這邊有點問題，會導致手機板無法在新分頁開圖和直接下載
-    //// 而且用分享選項得到的圖片還是沒有變大 哭啊
-    //   , {
-    //   width: node.clientWidth * scale,
-    //   height: node.clientHeight * scale,
-    //   style: {
-    //     transform: 'scale(' + scale + ')',
-    //     transformOrigin: 'top left'
-    //   }
-    // }
-  )
-    .then(function (dataUrl) {
-      let img = new Image();
-      img.src = dataUrl;
-      img.id = 'uesrCards'
-      document.body.appendChild(img);
+)
+
+cardContainer.addEventListener('click', (event) => {
+  if (event.target.tagName === ('IMG' || 'I')) {
+    toggleOwn(event.target.parentElement.dataset.id)
+    event.target.parentElement.dataset.own = !(event.target.parentElement.dataset.own === 'true')
+    renderResults()
+    localSave()
+  }
+
+})
+
+filterButton.addEventListener('click', (event) => {
+  if (event.target.parentElement.dataset.show === 'false') {
+    event.target.parentElement.dataset.show = 'true'
+    modal.dataset.show = 'true'
+    filterButton.innerHTML = `<p id="filter-label">Apply</p>`
+  }
+  else {
+    event.target.parentElement.dataset.show = 'false'
+    modal.dataset.show = 'false'
+    if (languageToUse === 'jp') {
+      filterButton.innerHTML = `<p id="filter-label">${multiLanguageArray.jp[4]}</p>`
+
+    } else {
+      filterButton.innerHTML = `<p id="filter-label">${multiLanguageArray.cn[4]}</p>`
+
+    }
+  }
+})
+
+modal.addEventListener('click', (event) => {
+  let filterSelected = undefined
+  if (event.target.classList.contains('filter') || event.target.parentElement.classList.contains('filter')) {
+    if (event.target.classList.contains('filter')) {
+      filterSelected = event.target
+    }
+    else if (event.target.parentElement.classList.contains('filter')) {
+      filterSelected = event.target.parentElement
+    }
+    filterSelected.dataset.select = !(filterSelected.dataset.select === 'true')
+
+  }
+  if (event.target.parentElement.classList.contains('group-logo')) {
+    event.target.parentElement.toggleAttribute('show')
+    const groupSelected = document.querySelectorAll(`.${event.target.parentElement.dataset.group}`)
+    groupSelected.forEach((idolFilter) => {
+      idolFilter.dataset.select = event.target.parentElement.hasAttribute('show')
     })
-    .catch(function (error) {
-      console.error('oops, something went wrong!', error);
-    });
-  exportButton.innerHTML = '<button onclick="exportImage()">↓重新曬卡↓</button>'
+  }
+  applyFilter()
+})
+
+filterAllButton.addEventListener('click', (event) => {
+  event.target.toggleAttribute('show')
+  idolFilters.forEach((idolFilter) => {
+    idolFilter.dataset.select = event.target.hasAttribute('show')
+  })
+  if (languageToUse === 'cn') {
+    filterAllButton.innerText = event.target.hasAttribute('show') ? 'Clear Idols' : 'All Idols'
+
+  } else {
+    filterAllButton.innerText = event.target.hasAttribute('show') ? '全取り消す' : '全アイドル選択'
+
+  }
+})
+
+selectAllButton.addEventListener('click', (event) => {
+  event.target.toggleAttribute('own')
+  let own = event.target.hasAttribute('own')
+  if (languageToUse === 'cn') {
+    selectAllButton.innerText = event.target.hasAttribute('own') ? '全部取消' : '全部選擇'
+  } else {
+    selectAllButton.innerText = event.target.hasAttribute('own') ? '全取り消す' : '全選択する'
+
+  }
+  cardsToRender.forEach((card) => {
+
+    card.own = own
+    playerCards[card.id].own = own
+  })
+  renderCards()
+  renderResults()
+
+
+})
+
+languageSelectButton.addEventListener('click', (event) => {
+
+  event.target.innerText = event.target.hasAttribute('jp') ? 'CN' : 'JP'
+
+
+  useLanguage(event.target.innerText)
+  event.target.toggleAttribute('jp')
+})
+
+function localSave() {
+  localStorage.setItem('recentCards', JSON.stringify(playerCards))
+  localStorage.setItem('recentVersion', newVersion)
+  localStorage.setItem('playerName', playerName)
+}
+
+function applyFilter() {
+  const filterObject = {
+    idol: [],
+    pool: [],
+    type: [],
+    job: [],
+  }
+  filters.forEach((f) => {
+    if (f.dataset.select === 'true') {
+      filterObject[f.dataset.key].push(f.dataset.value)
+    }
+  })
+  cardsToRender = []
+  playerCards.forEach((card) => {
+    if (filterObject.idol.includes(card.idol)
+      && filterObject.pool.includes(card.pool)
+      && filterObject.type.includes(card.type)
+      && filterObject.job.includes(card.job)) {
+      cardsToRender.push(card)
+    }
+  })
+  renderCards()
+  test()
+
+}
+
+function renderCards() {
+  cardContainer.innerHTML = ''
+  cardsToRender.forEach(card => {
+    cardContainer.innerHTML += `
+      <div class='card ${card.pool} ${card.idol} ${card.type} ${card.job}' data-own=${card.own} data-id=${card.id} >
+        <i class="fa-solid fa-circle-check "></i>
+        <img src="cards\\${card.image}" alt="" >
+      </div>
+    `
+  });
+}
+
+function renderResults() {
+  const resultArray = document.querySelectorAll('.percentage')
+  resultArray.forEach((percentage) => {
+    percentage.innerHTML = `${calcPlayerOwn(percentage.dataset.pool)}%`
+  })
+
+}
+
+function renderName(name) {
+  player.textContent = name
+}
+
+function useLanguage(language) {
+
+  if (language === 'CN') {
+    languageToUse = 'cn'
+  } else if (language === 'JP') {
+    languageToUse = 'jp'
+  } else {
+    if (sys === "zh-tw" || sys === "zh-cn" || sys === "zh-hk" || sys === "zh-sg" || sys === "zh") {
+      languageToUse = 'cn';
+    } else {
+      languageToUse = 'jp'
+    }
+  }
+  const languageArrayToUse = multiLanguageArray[languageToUse]
+
+  document.querySelector('#title').innerText = languageArrayToUse[0]
+  document.querySelector('#language').innerText = languageArrayToUse[1]
+  document.querySelector('#select-all').innerText = languageArrayToUse[2]
+  document.querySelector('#sorter').innerText = languageArrayToUse[3]
+  document.querySelector('#filter-label').innerText = languageArrayToUse[4]
+  document.querySelector('#five-stars-own').innerText = languageArrayToUse[5]
+  document.querySelector('#normal-percentage-label').innerText = languageArrayToUse[6]
+  document.querySelector('#limited-percentage-label').innerText = languageArrayToUse[7]
+  document.querySelector('#fes-percentage-label').innerText = languageArrayToUse[8]
+  document.querySelector('#all-percentage-label').innerText = languageArrayToUse[9]
+  document.querySelector('#normal').innerText = languageArrayToUse[6]
+  document.querySelector('#limited').innerText = languageArrayToUse[7]
+  document.querySelector('#fes').innerText = languageArrayToUse[8]
+  document.querySelector('#special').innerText = languageArrayToUse[10]
+  document.querySelector('#scorer-label').innerText = languageArrayToUse[11]
+  document.querySelector('#buffer-label').innerText = languageArrayToUse[12]
+  document.querySelector('#supporter-label').innerText = languageArrayToUse[13]
+  document.querySelector('#filter-all').innerText = languageArrayToUse[14]
+  document.querySelector('#alert-1').innerHTML = languageArrayToUse[15]
+  document.querySelector('#alert-2').innerHTML = languageArrayToUse[16]
+  document.querySelector('#alert-3').innerHTML = languageArrayToUse[17]
+
+}
+
+function calcPlayerOwn(pool) {
+  if (pool === 'all') {
+    return Math.round(playerCards.filter(card => (card.own === true)).length / playerCards.length * 100)
+  }
+  else {
+    return Math.round(playerCards.filter(card => ((card.own === true) && (card.pool === pool))).length / playerCards.filter(card => (card.pool === pool)).length * 100)
+  }
+
+}
+
+function toggleOwn(id) {
+  let toggleCard = playerCards.find((card) => {
+    return (Number.parseInt(card.id) === Number.parseInt(id))
+  })
+  toggleCard.own = !(toggleCard.own)
+}
+
+document.querySelector('#save').addEventListener('click', (event) => {
+  document.querySelector('#alert').toggleAttribute('show')
+  document.querySelector("#bottom-navigator ").style.position = 'absolute'
+  document.querySelector("#bottom-navigator ").style.bottom = '-100vh'
+  const capture = document.querySelector('#capture')
+  let scale = 6
+  domtoimage.toPng(capture, {
+    width: capture.clientWidth * scale,
+    height: capture.clientHeight * scale,
+    style: {
+      transform: 'scale(' + scale + ')',
+      transformOrigin: 'top left'
+    }
+  })
+    .then(function (dataUrl) {
+      let fakeBtn = document.querySelector('#test')
+      fakeBtn.innerHTML = `
+        <a id="download" href="${dataUrl}" download>DOWNLOAD</a>
+      `
+    })
+    .then(() => {
+      document.querySelector('#download').click()
+      window.location.reload()
+      window.alert('Thank You for Waiting!')
+    })
+
+})
+
+function test() {
+  playerCards.forEach((card) => {
+    if ((card.job !== 'supporter') && (card.job !== 'scorer') && (card.job !== 'buffer')) {
+      console.log(card)
+    }
+  })
 }
